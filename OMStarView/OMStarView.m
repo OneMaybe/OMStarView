9ee2fa350b6e87d8c857784d3adaa22ee11b4efa
KellenYang <bcmac3@bcmac3deiMac.local>
2015-08-05 11:14:02 +0800

    Start

---
+++ OMStarView/OMStarView.m
@@ -0,0 +1,13 @@
+//
+//  OMStarView.m
+//  OMStarViewDemo
+//
+//  Created by bcmac3 on 15/7/14.
+//  Copyright (c) 2015年 OneMaybe. All rights reserved.
+//
+
+#import "OMStarView.h"
+
+@implementation OMStarView
+
+@end
